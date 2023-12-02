4e3cf85a746e2e93d1be58b02fb5b670074a6bdb
David Stroupe <d.n.stroupe@gmail.com>
2023-12-02 13:19:43 -1000

    updated

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Unit2.PuppyBowl.Starter.
+Puppy Bowl Project
+
+Overview
+
+The Puppy Bowl project is a delightful web application that showcases adorable participants of a fictional Puppy Bowl event. It allows users to interactively view, add, and remove puppy players. Built with HTML, CSS, and JavaScript, this project offers a practical demonstration of fundamental web development concepts.
+
+Features
+
+View Players: Displays a list of all puppy players participating in the Puppy Bowl.
+Add New Players: Provides a form for users to add new players to the event.
+Remove Players: Enables users to remove players from the list.
+
+Setup
+
+To set up the project on your local machine, follow these steps:
+
+1. Clone the Repository:
+   Execute the following command in your terminal:
+   git clone https://github.com/dnstroupe/Unit2.PuppyBowl.Starter.git
+
+2. Navigate to the Project Directory:
+   Change into the project directory using:
+   cd Unit2.PuppyBowl.Starter
+
+3. Open the Project:
+   Open the project in your preferred code editor. If you're using Visual Studio Code, type code . in the terminal within the project directory to open the project.
+
+Usage
+
+Interact with the Application: Open the index.html file in a web browser.
+Add New Players: Use the form at the top of the page to submit new puppy players.
+Remove Players: Click on the "Remove from roster" button on any player card to remove the player from the list.
+
+Technologies Used
+
+HTML: Structure of the web pages.
+CSS: Styling of the web pages.
+JavaScript: Interactive elements and API integration.
+
+Contributing
+
+Contributions to the Puppy Bowl project are encouraged. Please adhere to standard coding conventions and maintain the integrity of the existing codebase.
+
+Contributors
+Stephen Carouthers (@sjcarouthers)
+Andrew King (@andaking3424)
--- script.js
+++ script.js
@@ -1,27 +1,57 @@
-const playerContainer = document.getElementById('all-players-container');
-const newPlayerFormContainer = document.getElementById('new-player-form');
-const cohortName = '2310-FSA-ET-WEB-PT';
+const playerContainer = document.getElementById("all-players-container");
+const newPlayerFormContainer = document.getElementById("new-player-form");
+const cohortName = "2310-FSA-ET-WEB-PT-SF";
 const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
 
 const fetchAllPlayers = async () => {
-    try {
-        const response = await fetch(`${APIURL}players`);
-        const { data } = await response.json();
-        return data.players;
-    } catch (err) {
-        console.error('Uh oh, trouble fetching players!', err);
-    }
+  try {
+    const response = await fetch(`${APIURL}players`);
+    const { data } = await response.json();
+    return data.players;
+  } catch (err) {
+    console.error("Uh oh, trouble fetching players!", err);
+  }
 };
+
+const fetchSinglePlayer = async (playerId) => {
+  try {
+    const response = await fetch(`${APIURL}players/${playerId}`);
+    const { data } = await response.json();
+    console.log(data.player);
+  } catch (err) {
+    console.error(`Oh no, trouble fetching player #${playerId}!`, err);
+  }
+};
+
+const addNewPlayer = async (playerObj) => {
+  try {
+    const response = await fetch(`${APIURL}players`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(playerObj),
+    });
+    return response.json();
+  } catch (err) {
+    console.error("Oops, something went wrong with adding that player!", err);
+  }
+};
+
 const removePlayer = async (playerId) => {
-    try {
-        await fetch(`${APIURL}players/${playerId}`, { method: 'DELETE' });
-    } catch (err) {
-        console.error(`Whoops, trouble removing player #${playerId} from the roster!`, err);
-    };
-    
+  try {
+    await fetch(`${APIURL}players/${playerId}`, { method: "DELETE" });
+  } catch (err) {
+    console.error(
+      `Whoops, trouble removing player #${playerId} from the roster!`,
+      err
+    );
+  }
+};
+
 const renderAllPlayers = (playerList) => {
-    try {
-        let playerContainerHTML = playerList.map(player => `
+  try {
+    let playerContainerHTML = playerList
+      .map(
+        (player) => `
             <div class="player-card">
                 <img src="${player.imageUrl}" alt="${player.name}">
                 <h3>${player.name}</h3>
@@ -30,9 +60,44 @@ const renderAllPlayers = (playerList) => {
                 <button onclick="fetchSinglePlayer(${player.id})">See details</button>
                 <button onclick="removePlayer(${player.id})">Remove from roster</button>
             </div>
-        `).join('');
-        playerContainer.innerHTML = playerContainerHTML;
-    } catch (err) {
-        console.error('Uh oh, trouble rendering players!', err);
-    }
+        `
+      )
+      .join("");
+    playerContainer.innerHTML = playerContainerHTML;
+  } catch (err) {
+    console.error("Uh oh, trouble rendering players!", err);
+  }
 };
+
+const renderNewPlayerForm = () => {
+  try {
+    const formHTML = `
+            <form id="new-player">
+                <input type="text" id="name" placeholder="Name" required>
+                <input type="text" id="breed" placeholder="Breed" required>
+                <button type="submit">Add New Player</button>
+            </form>
+        `;
+    newPlayerFormContainer.innerHTML = formHTML;
+    document
+      .getElementById("new-player")
+      .addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const name = document.getElementById("name").value;
+        const breed = document.getElementById("breed").value;
+        await addNewPlayer({ name, breed });
+        const players = await fetchAllPlayers();
+        renderAllPlayers(players);
+      });
+  } catch (err) {
+    console.error("Uh oh, trouble rendering the new player form!", err);
+  }
+};
+
+const init = async () => {
+  const players = await fetchAllPlayers();
+  renderAllPlayers(players);
+  renderNewPlayerForm();
+};
+
+init();
